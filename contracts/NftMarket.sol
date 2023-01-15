293d7580d52b8a825c3b27012db9e739af374f47
walid horchani <walid.horchani@oyez.fr>
2023-01-15 22:59:34 +0100

    NFT-16 vid28 fix

--- contracts/NftMarket.sol
+++ contracts/NftMarket.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.4.22 <0.9.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract NftMarket is ERC721 {
+contract NftMarket is ERC721URIStorage {
   constructor() ERC721("NFTdos","NFTDOS") {
   }
 }
