0adf2f3c03bea979c0efaff9b4cf63bd04e09dfa
walid horchani <walid.horchani@oyez.fr>
2023-01-16 01:09:17 +0100

    NFT-17 load contract

--- src/pages/nft/Listing.jsx
+++ src/pages/nft/Listing.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { useWeb3Provider } from "../../context/Web3ProviderContext";
 
 function Listing() {
-  const { provider } = useWeb3Provider();
+  const { provider, ...rest } = useWeb3Provider();
+  console.log("🚀 ~ file: Listing.jsx:6 ~ Listing ~ rest", rest);
   async function getAccounts() {
     const x = await provider?.listAccounts();
     console.log("🚀 ~ file: Listing.jsx:6 ~ Listing ~ res", x);
   }
   getAccounts();
-  return <div>Listing</div>;
+  return <div>Listing </div>;
 }
 
 export default Listing;
